2b068ff70bfb845cb4406154bef43bbdd6ade3ea
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-18 05:31:45 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cvbgh
+admin.site.register(Cvbgh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Cvbgh(models.Model):
+    'Generated Model'
+    nvjgj = models.BigIntegerField()
+    yturu = models.BigIntegerField()
